8a8025c2fb355008278238a4695ae10ef9db7620
BivasBiswas <bivasbiswas1999@gmail.com>
2021-07-01 00:27:19 +0530

    day 15 done

--- day-13/day13.js
+++ day-13/day13.js
@@ -1,26 +1,28 @@
-const article = document.querySelector("article")
-const searchBtn = document.querySelector('button')
+const article = document.querySelector('article')
 const inputEl = document.querySelector('input')
+const searchBtn = document.querySelector('button')
 
-searchBtn.addEventListener('click', fetchDogs)
-fetchDogs()
+searchBtn.addEventListener('click', fetchdata)
 
-function fetchDogs(){
+function fetchdata() {
     article.innerHTML = ''
     fetch(`https://dog.ceo/api/breed/${inputEl.value}/images/random/10`)
-        .then((res) => res.json())
-        .then((data) =>{
-            getResponce(data)
-        })
+        .then(res => res.json())
+        .then(data => {
+            getresponse(data)
+            // console.log(data)
+            })
 }
 
-function getResponce(data){
-    if(data.status === 'success'){
-        data.message.forEach((image) => {
-            article.innerHTML += `<img src=${image} alt="">`
+fetchdata()
+function getresponse(data){
+    if(data.status==='success') {
+        data.message.forEach((ima) => {
+            article.innerHTML += `<img src = ${ima} alt="">`
+            console.log(ima)
         })
     }
-    else{
-        article.innerHTML = `<h1> Not Found the dog types</h1>`
+    else {
+        article.innerHTML = `<h1> hksdafkjfdskj </h1>`
     }
 }
