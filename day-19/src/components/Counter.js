20404e2fc27b7871a3cd5e9f950fe3c8fb61e65b
BivasBiswas <bivasbiswas1999@gmail.com>
2021-07-09 21:11:24 +0530

    day19 done

--- day-19/src/components/Counter.js
+++ day-19/src/components/Counter.js
@@ -1,67 +1,66 @@
 import React, { Component } from "react";
-// import { useState } from "react";
+import { useState } from "react";
 import "../styles/Counter.css";
 
 // class components
-export class Counter extends Component {
-    constructor(){
-        super()
-        this.state = {
-            count : 0
-        }
-    }
+// export class Counter extends Component {
+//     constructor(){
+//         super()
+//         this.state = {
+//             count : 0
+//         }
+//     }
 
-    increment(){
-        this.setState({
-            count : this.state.count + 1
-        })
-    }
+//     increment(){
+//         this.setState({
+//             count : this.state.count + 1
+//         })
+//     }
 
-    decrement(){
-        this.setState({
-            count: this.state.count - 1
-        })
-    }
+//     decrement(){
+//         this.setState({
+//             count: this.state.count - 1
+//         })
+//     }
 
-    reset(){
-        this.setState({
-            count : 0
-        })
-    }
+//     reset(){
+//         this.setState({
+//             count : 0
+//         })
+//     }
 
-    render() {
-        return (
-            <>
-                <h1>{this.state.count}</h1>
-                <div className="btn">
-                    <button onClick={()=>this.increment()}>Increment</button>
-                    <button onClick={()=>this.decrement()}>Decrement</button>
-                    <button onClick={()=>this.reset()}>Rest</button>
-                </div>
-            </>
-        )
-    }
-}
+//     render() {
+//         return (
+//             <>
+//                 <h1>{this.state.count}</h1>
+//                 <div className="btn">
+//                     <button onClick={()=>this.increment()}>Increment</button>
+//                     <button onClick={()=>this.decrement()}>Decrement</button>
+//                     <button onClick={()=>this.reset()}>Rest</button>
+//                 </div>
+//             </>
+//         )
+//     }
+// }
 
-export default Counter
+// export default Counter
 
 // functional components
 
-// const Counter = () => {
-//     let [count, quries] = useState(0)
+const Counter = () => {
+  let [count, setCount] = useState(0);
 
+  return (
+    <>
+      <h1>{count}</h1>
 
-//   return (
-//     <>
-//       <h1>{ count }</h1>
-      
-//       <div className="btn">
-//         <button onClick={() => quries(count+1)}>Increment</button>
-//         <button onClick={() => quries(count-1)}>Decrement</button>
-//         <button onClick={() => quries(count = 0)}>Rest</button>
-//       </div>
-//     </>
-//   );
-// };
+      <div className="btn">
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button onClick={() => setCount((count = 0))}>Rest</button>
+      </div>
+    </>
+  );
+};
 
-// export default Counter;
+export default Counter;
